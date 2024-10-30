6f77b87530c904820757d05af86c64294bb52ccb
mobin <mobinizadi.dev@gmail.com>
2024-10-30 19:10:18 +0330

    Update the dark mode settings

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,13 @@
 // theme handler
 const themeHandlerButtons = document.querySelectorAll('.theme-mode')
+const modal = document.querySelector('.modal')
+const modalClosesBtn = document.querySelectorAll('.modal__close-btn')
+const modalAgreeBtn = document.querySelector('.modal__agree-btn')
+
 
 
 // +++++++++++++++++++++++++++++++++++function++++++++++++++++++++++++++
+// theme handler
 function themeModeHandler(event) {
     let mode = event.target.dataset.mode
     if (mode === 'light') {
@@ -12,7 +17,10 @@ function themeModeHandler(event) {
         document.documentElement.classList.add('dark')
         localStorage.setItem('dark', true)
     }
-
+}
+// modal show control
+function modalShowControl() {
+    localStorage.setItem('showModal', false)
 }
 // +++++++++++++++++++++++++++++++++++event++++++++++++++++++++++++++
 
@@ -20,16 +28,41 @@ function themeModeHandler(event) {
 themeHandlerButtons.forEach(button => {
     button.addEventListener('click', themeModeHandler)
 })
-
+// load
 window.addEventListener('load', () => {
+    // dark mode
     let darkMode = localStorage.getItem('dark') == 'true' ? true : false
     let prefersUserTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
-
-    if (darkMode || prefersUserTheme) {
+    let showModal = localStorage.getItem('showModal') == 'false' ? true : false
+    if (showModal) {
+        prefersUserTheme = false
+    }
+    if (prefersUserTheme) {
+        modal.classList.remove('hidden')
+    } else {
+        modal.classList.add('hidden')
+    }
+    if (darkMode) {
         document.documentElement.classList.add('dark')
+        modal.classList.add('hidden')
     } else {
         document.documentElement.classList.remove('dark')
     }
 
 
+})
+
+// modal close
+modalClosesBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+        modalShowControl()
+        modal.classList.add('hidden')
+    })
+})
+// modal  agree btn
+modalAgreeBtn.addEventListener('click', () => {
+    modalShowControl()
+    modal.classList.add('hidden')
+    localStorage.setItem('dark', true)
+    document.documentElement.classList.add('dark')
 })
