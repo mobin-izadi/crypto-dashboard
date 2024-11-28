337abb0f01a39d829f4857a4da6debc524c0dc06
mobin <mobinizadi.dev@gmail.com>
2024-11-28 15:23:09 +0330

    Add to the transactions table on the transactions page

--- public/scripts/transactions.js
+++ public/scripts/transactions.js
@@ -1,22 +1,40 @@
 // filter
 const filterBtn = document.getElementById('filter-btn')
 const filterWrapper = document.querySelector('.filter-wrapper')
+const filterDayBtns = document.querySelectorAll('.filter__btn')
 
 
 
 
 // functions
+
+// open && close filter Wrapper
 function filterWrapperShowHandler() {
     filterWrapper.classList.toggle('hidden')
+}
+// change filter day
+function filterDauHandler(mainBtn) {
+    filterDayBtns.forEach(btn => {
+        btn.classList.remove('text-purple-500')
+        btn.classList.remove('bg-purple-200')
+    })
+
+    mainBtn.classList.add('text-purple-500')
+    mainBtn.classList.add('bg-purple-200')
 }
 
 
 
 // events
+// open && close filter Wrapper
 filterBtn.addEventListener('click', filterWrapperShowHandler)
 document.addEventListener('click', (event) => {
     if (!filterWrapper.contains(event.target) && !filterBtn.contains(event.target)) {
         filterWrapper.classList.add('hidden')
     }
 
+})
+// change filter day
+filterDayBtns.forEach(btn => {
+    btn.addEventListener('click', (event) => filterDauHandler(event.target))
 })
